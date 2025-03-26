d90d360b48681a7b9f39749ba9b769178b58a3d9
Joshua <joshyarit07@gmail.com>
2025-03-26 17:08:14 -0600

    Refactor SignUpScreen to enhance form validation and improve UI components

--- app/src/main/java/com/example/bravia/presentation/ui/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/bravia/presentation/ui/screens/SignUpScreen.kt
@@ -1,6 +1,7 @@
 package com.example.bravia.presentation.ui.screens
 
-import android.content.res.Resources
+
+import android.util.Patterns
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -11,8 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -30,33 +34,36 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.example.bravia.presentation.ui.layout.MainLayout
 import com.example.studentapp.presentation.ui.theme.ThemeDefaults
 import com.example.bravia.presentation.ui.theme.*
-import com.example.bravia.presentation.viewmodel.InternshipViewModel
 import com.example.bravia.presentation.viewmodel.SignupViewModel
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+
+
 @Composable
 fun SignUpScreen(
     paddingValues: PaddingValues,
     signUpViewModel: SignupViewModel
 ) {
     var email by remember { mutableStateOf("") }
+    var isEmailValid by remember { mutableStateOf(false) }
+
     var password by remember { mutableStateOf("") }
+    var isPasswordValid by remember { mutableStateOf(false) }
+    var passwordVisible by remember { mutableStateOf(false) }
+
     var confirmPassword by remember { mutableStateOf("") }
-    var expanded by remember { mutableStateOf(false) }
-    val options = listOf("Student", "Business")
-    var selectedOption by remember { mutableStateOf("Select option") }
+    var isConfirmPasswordValid by remember { mutableStateOf(false) }
+    var confirmPasswordVisible by remember { mutableStateOf(false) }
+
+
+    var isSelectedOptionValid by remember { mutableStateOf(false) }
 
     Column (
         modifier = Modifier
@@ -93,127 +100,265 @@ fun SignUpScreen(
 
             Spacer(modifier = Modifier.height(ThemeDefaults.spacerHeight))
 
-            Text(
-                modifier = Modifier.padding(start = ThemeDefaults.textPadding),
-                text = "Email",
-                style = Typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = ThemeDefaults.textFieldPadding)
+            Email(
+                email = email,
+                onEmailChange = {
+                    email = it
+                    isEmailValid = Patterns.EMAIL_ADDRESS.matcher(it).matches()
+                },
+                isValid = isEmailValid
             )
 
             Spacer(modifier = Modifier.height(ThemeDefaults.spacerHeight))
 
-            Text(
-                modifier = Modifier.padding(start = ThemeDefaults.textPadding),
-                text = "Password",
-                style = Typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = ThemeDefaults.textFieldPadding)
+            Password(
+                password = password,
+                onPasswordChange = {
+                    password = it
+                    isPasswordValid = it.length >= 8
+                },
+                passwordVisible = passwordVisible,
+                onPasswordVisibilityChange = { passwordVisible = !passwordVisible },
+                isValid = isPasswordValid
             )
 
             Spacer(modifier = Modifier.height(ThemeDefaults.spacerHeight))
 
-            Text(
-                modifier = Modifier.padding(start = ThemeDefaults.textPadding),
-                text = "Confirm Password",
-                style = Typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            OutlinedTextField(
-                value = confirmPassword,
-                onValueChange = { confirmPassword = it },
-                visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = ThemeDefaults.textFieldPadding)
+            ConfirmPassword(
+                confirmPassword = confirmPassword,
+                onConfirmPasswordChange = {
+                    confirmPassword = it
+                    isConfirmPasswordValid = it == password
+                },
+                confirmPasswordVisible = confirmPasswordVisible,
+                onConfirmPasswordVisibilityChange = { confirmPasswordVisible = !confirmPasswordVisible },
+                isValid = isConfirmPasswordValid
             )
 
             Spacer(modifier = Modifier.height(ThemeDefaults.spacerHeight))
 
-            Text(
-                modifier = Modifier.padding(start = ThemeDefaults.textPadding),
-                text = "Type of account",
-                style = Typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            ExposedDropdownMenuBox(
-                expanded = expanded,
-                onExpandedChange = { expanded = !expanded }
-            ) {
-
-                OutlinedTextField(
-                    value = selectedOption,
-                    onValueChange = {},
-                    readOnly = true,
-                    trailingIcon = {
-                        Icon(Icons.Default.ArrowDropDown, contentDescription = null)
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(ThemeDefaults.textFieldPadding)
-                        .clickable { expanded = true }
-                        .menuAnchor()
-                )
-                ExposedDropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
-                ) {
-                    options.forEach { option ->
-                        DropdownMenuItem(
-                            text = { Text(option) },
-                            onClick = {
-                                selectedOption = option
-                                expanded = false
-                            }
-                        )
-                    }
-                }
-            }
-
-        }
-
-        Spacer(modifier = Modifier.height(ThemeDefaults.spacerHeight))
-
-        // Button
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 70.dp)
-        ) {
-            Text(
-                text = "Continue ...",
-                style = Typography.headlineSmall,
+            AccountType(
+                isSelectedOptionValid = isSelectedOptionValid,
+                onSelectedOptionValidChange = { isSelectedOptionValid = it }
             )
 
         }
 
         Spacer(modifier = Modifier.height(ThemeDefaults.spacerHeight))
 
-        Button(
-            onClick = { /* TODO Acción de ir a login */ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 50.dp)
-        ) {
-            Text(
-                text = "Login",
-                style = Typography.headlineSmall,
-            )
-        }
+        ContinueButton(
+            isValidEmail = isEmailValid,
+            isValidPassword = isPasswordValid,
+            isValidConfirmPassword = isConfirmPasswordValid,
+            isSelectedOptionValid = isSelectedOptionValid
+        )
+
+        Spacer(modifier = Modifier.height(ThemeDefaults.spacerHeight))
+
+        RedirectToLogin()
 
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AccountType(
+    isSelectedOptionValid: Boolean,
+    onSelectedOptionValidChange : (Boolean) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var options = listOf("Student", "Business")
+    var selectedOption by remember { mutableStateOf("Select option") }
+
+    Text(
+        modifier = Modifier.padding(start = ThemeDefaults.textPadding),
+        text = "Type of account",
+        style = Typography.headlineSmall,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = it },
+    ) {
+
+        OutlinedTextField(
+            value = selectedOption,
+            onValueChange = {},
+            readOnly = true,
+            trailingIcon = {
+                Icon(Icons.Default.ArrowDropDown, contentDescription = null)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(ThemeDefaults.textFieldPadding)
+                .clickable { expanded = true }
+                .menuAnchor()
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(option) },
+                    onClick = {
+                        selectedOption = option
+                        expanded = false
+                        onSelectedOptionValidChange(selectedOption != "Select option")
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RedirectToLogin(
+
+) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp),
+        onClick = { /* TODO Acción de ir a login */ },
+        enabled = true
+    ) {
+        Text(
+            text = "Login",
+            style = Typography.headlineSmall,
+        )
+    }
+}
+
+@Composable
+fun ContinueButton(
+    isValidEmail: Boolean,
+    isValidPassword: Boolean,
+    isValidConfirmPassword: Boolean,
+    isSelectedOptionValid: Boolean
+) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 70.dp),
+        onClick = { /*TODO*/ },
+        enabled = isValidEmail && isValidPassword && isValidConfirmPassword && isSelectedOptionValid
+    ) {
+        Text(
+            text = "Continue ...",
+            style = Typography.headlineSmall,
+        )
+
+    }
+}
+
+
+@Composable
+fun ConfirmPassword(
+    confirmPassword: String,
+    onConfirmPasswordChange: (String) -> Unit,
+    confirmPasswordVisible: Boolean,
+    onConfirmPasswordVisibilityChange: () -> Unit,
+    isValid: Boolean
+) {
+    Text(
+        modifier = Modifier.padding(start = ThemeDefaults.textPadding),
+        text = "Confirm Password",
+        style = Typography.headlineSmall,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = ThemeDefaults.textFieldPadding),
+        value = confirmPassword,
+        onValueChange = onConfirmPasswordChange,
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val icon = if (confirmPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+            Icon(
+                imageVector = icon,
+                contentDescription = "Toggle password visibility",
+                modifier = Modifier.clickable(onClick = onConfirmPasswordVisibilityChange)
+            )
+        },
+        visualTransformation = if (confirmPasswordVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        isError = !isValid
+    )
+}
+
+
+@Composable
+fun Password(
+    password: String,
+    onPasswordChange: (String) -> Unit,
+    passwordVisible: Boolean,
+    onPasswordVisibilityChange: () -> Unit,
+    isValid: Boolean
+) {
+    Text(
+        modifier = Modifier.padding(start = ThemeDefaults.textPadding),
+        text = "Password",
+        style = Typography.headlineSmall,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = ThemeDefaults.textFieldPadding),
+        value = password,
+        onValueChange = onPasswordChange,
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val icon = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+            Icon(
+                imageVector = icon,
+                contentDescription = "Toggle password visibility",
+                modifier = Modifier.clickable(onClick = onPasswordVisibilityChange)
+            )
+        },
+        visualTransformation = if (passwordVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        isError = !isValid
+    )
+}
+
+@Composable
+fun Email(
+    email: String,
+    onEmailChange: (String) -> Unit,
+    isValid: Boolean
+) {
+    Text(
+        modifier = Modifier.padding(start = ThemeDefaults.textPadding),
+        text = "Email",
+        style = Typography.headlineSmall,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = ThemeDefaults.textFieldPadding),
+        value = email,
+        onValueChange = onEmailChange,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        maxLines = 1,
+        singleLine = true,
+        isError = !isValid
+    )
+}
+
+
+
