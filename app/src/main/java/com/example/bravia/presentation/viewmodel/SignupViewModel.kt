f37205df18c1593b41ec1ed4c244404be348047a
Joshua <joshyarit07@gmail.com>
2025-03-27 10:45:58 -0600

    Refactor SignupViewModel to simplify state management and remove unused use cases

--- app/src/main/java/com/example/bravia/presentation/viewmodel/SignupViewModel.kt
+++ app/src/main/java/com/example/bravia/presentation/viewmodel/SignupViewModel.kt
@@ -4,9 +4,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.bravia.domain.usecase.GetAllInterestUseCase
-import com.example.bravia.domain.usecase.GetInterestByIdUseCase
-import kotlinx.coroutines.flow.StateFlow
 
 sealed class SignUPState {
     data class Loading(val message: String) : SignUPState()
@@ -15,15 +12,27 @@ sealed class SignUPState {
 }
 
 class SignupViewModel (
-    private val getAllInterestsUseCase: GetAllInterestUseCase,
-    private val getInterestByIdUseCase: GetInterestByIdUseCase
+
 ) : ViewModel() {
+    var email by mutableStateOf("")
+        private set
 
+    var password by mutableStateOf("")
+        private set
 
+    var confirmPassword by mutableStateOf("")
+        private set
 
-    fun findAllInterests() {
-        val interests = getAllInterestsUseCase()
-        // Handle the list of interests as needed
+    fun onEmailChange(email: String) {
+        this.email = email
+    }
+
+    fun onPasswordChange(password: String) {
+        this.password = password
+    }
+
+    fun onConfirmPasswordChange(confirmPassword: String) {
+        this.confirmPassword = confirmPassword
     }
 
     fun signUp() {
